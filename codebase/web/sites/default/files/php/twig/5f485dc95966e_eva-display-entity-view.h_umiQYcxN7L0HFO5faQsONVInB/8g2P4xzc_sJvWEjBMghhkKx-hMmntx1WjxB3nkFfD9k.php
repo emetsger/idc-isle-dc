6e7af95990c289e3e29a0af46712e14c8592f500
Elliot Metsger <emetsger@jhu.edu>
2020-08-27 21:39:48 -0400

    Install modules and JS libraries, and place them under version control.
    This is the result of:
    docker-compose up -d
    docker-compose exec drupal with-contenv bash -lc 'COMPOSER_MEMORY_LIMIT=-1 composer install -v'
    make install
    make hydrate
    
    Then installing Masonry:
    docker-compose exec drupal with-contenv bash -lc 'COMPOSER_MEMORY_LIMIT=-1 composer require drupal/masonry'
    docker-compose exec drupal with-contenv bash -lc 'COMPOSER_MEMORY_LIMIT=-1 composer require drupal/masonry_views'
    (codebase/web/sites/default/libraries/masonry/dist) curl https://unpkg.com/masonry-layout@4.2.2/dist/masonry.pkgd.min.js -s -o masonry.pkgd.min.js
    (codebase/web/sites/default/libraries/imagesloaded) curl https://unpkg.com/imagesloaded@4.1.4/imagesloaded.pkgd.min.js -s -o imagesloaded.pkgd.min.js
    Finally, install two Masonry modules from https://islandora-idc.traefik.me/admin/modules

---
+++ codebase/web/sites/default/files/php/twig/5f485dc95966e_eva-display-entity-view.h_umiQYcxN7L0HFO5faQsONVInB/8g2P4xzc_sJvWEjBMghhkKx-hMmntx1WjxB3nkFfD9k.php
@@ -0,0 +1,178 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* modules/contrib/eva/templates/eva-display-entity-view.html.twig */
+class __TwigTemplate_c8dd57d2525c0b714d3f6b0b836c7874df77f24abc951eecf8cb09fe69480419 extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = ["set" => 40, "if" => 51];
+        $filters = ["clean_class" => 43, "escape" => 49];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['set', 'if'],
+                ['clean_class', 'escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 40
+        $context["classes"] = [0 => "view", 1 => "view-eva", 2 => ("view-" . \Drupal\Component\Utility\Html::getClass($this->sandbox->ensureToStringAllowed(        // line 43
+($context["id"] ?? null)))), 3 => ("view-id-" . $this->sandbox->ensureToStringAllowed(        // line 44
+($context["id"] ?? null))), 4 => ("view-display-id-" . $this->sandbox->ensureToStringAllowed(        // line 45
+($context["display_id"] ?? null))), 5 => ((        // line 46
+($context["dom_id"] ?? null)) ? (("js-view-dom-id-" . $this->sandbox->ensureToStringAllowed(($context["dom_id"] ?? null)))) : (""))];
+        // line 49
+        echo "<div";
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["attributes"] ?? null), "addClass", [0 => ($context["classes"] ?? null)], "method")), "html", null, true);
+        echo ">
+  ";
+        // line 50
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["title_prefix"] ?? null)), "html", null, true);
+        echo "
+  ";
+        // line 51
+        if (($context["title"] ?? null)) {
+            // line 52
+            echo "    ";
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["title"] ?? null)), "html", null, true);
+            echo "
+  ";
+        }
+        // line 54
+        echo "  ";
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["title_suffix"] ?? null)), "html", null, true);
+        echo "
+
+  ";
+        // line 56
+        if (($context["header"] ?? null)) {
+            // line 57
+            echo "    <header>
+      ";
+            // line 58
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["header"] ?? null)), "html", null, true);
+            echo "
+    </header>
+  ";
+        }
+        // line 61
+        echo "
+  ";
+        // line 62
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["attachment_before"] ?? null)), "html", null, true);
+        echo "
+
+  ";
+        // line 64
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["rows"] ?? null)), "html", null, true);
+        echo "
+  ";
+        // line 65
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["empty"] ?? null)), "html", null, true);
+        echo "
+  ";
+        // line 66
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["pager"] ?? null)), "html", null, true);
+        echo "
+
+  ";
+        // line 68
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["attachment_after"] ?? null)), "html", null, true);
+        echo "
+  ";
+        // line 69
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["more"] ?? null)), "html", null, true);
+        echo "
+
+  ";
+        // line 71
+        if (($context["footer"] ?? null)) {
+            // line 72
+            echo "    <footer>
+      ";
+            // line 73
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["footer"] ?? null)), "html", null, true);
+            echo "
+    </footer>
+  ";
+        }
+        // line 76
+        echo "
+  ";
+        // line 77
+        if (($context["feed_icons"] ?? null)) {
+            // line 78
+            echo "  ";
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["feed_icons"] ?? null)), "html", null, true);
+            echo "
+  ";
+        }
+        // line 80
+        echo "</div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "modules/contrib/eva/templates/eva-display-entity-view.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  147 => 80,  141 => 78,  139 => 77,  136 => 76,  130 => 73,  127 => 72,  125 => 71,  120 => 69,  116 => 68,  111 => 66,  107 => 65,  103 => 64,  98 => 62,  95 => 61,  89 => 58,  86 => 57,  84 => 56,  78 => 54,  72 => 52,  70 => 51,  66 => 50,  61 => 49,  59 => 46,  58 => 45,  57 => 44,  56 => 43,  55 => 40,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "modules/contrib/eva/templates/eva-display-entity-view.html.twig", "/var/www/drupal/web/modules/contrib/eva/templates/eva-display-entity-view.html.twig");
+    }
+}
